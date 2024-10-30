93b913b8f0f4480d146d7c84d806223a4934660f
AngelMVG <verduzcogonzalezangelmanuel3@gmail.com>
2024-10-29 18:15:42 -0600

    Creation of home page and page protection using authService

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../services/auth/auth.service';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,27 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(private router:Router, private authService: AuthService) { }
+  
+  goToTransacciones() {
+    this.router.navigate(['/transacciones'])
+  }
+
+  goToPresupuestos() {
+    this.router.navigate(['/presupuestos'])
+  }
+
+  goToCategorias() {
+    this.router.navigate(['/categorias'])
+  }
+
+  goToAnalisis() {
+    this.router.navigate(['/analisis'])
+  }
+
+  logout() {
+    this.authService.logout()
+    this.router.navigate(['/login'])
+  }
 
 }
